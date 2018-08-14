52da93627cb95d8e84cd78a6a26522bacd85676e
Dathan Bennett <dathan@udacity.com>
2018-08-13 20:11:17 -0700

    Change first migration to question/answer design
    It's a poor practice, I know, but nobody else is using this code yet --
    it'll be fine.

--- db/migrations/001_init-db.sql
+++ db/migrations/001_init-db.sql
@@ -1,36 +1,28 @@
 -- +migrate Up
 -- +migrate StatementBegin
-create table orders (
-  id uuid primary key,
-  customer text,
-  created_at timestamp,
-  updated_at timestamp
+create table users (
+  id integer primary key,
+  display_name text
 );
 
-create table line_items (
-  id uuid primary key,
-  order_id uuid not null,
-  product_id uuid not null,
-  created_at timestamp,
-  updated_at timestamp
+create table posts (
+  id integer primary key,
+  post_type integer not null,
+  post_type_id id not null,
+  body text not null,
+  user_id integer not null references users(id)
 );
 
-create table products (
-  id uuid primary key,
-  name text not null,
-  description text not null,
-  amount integer not null,
-  created_at timestamp,
-  updated_at timestamp
+create table comments (
+  id integer primary key,
+  post_id integer not null references posts(id),
+  "text" text not null,
+  user_id integer not null references users(id)
 );
-
-create index product_name_index on products using gin(to_tsvector('english', "name"));
-create index product_description_index on products using gin(to_tsvector('english', description));
 -- +migrate StatementEnd
 
 -- +migrate Down
 -- +migrate StatementBegin
-drop table line_items;
-drop table orders;
-drop table skus;
+drop table users;
+drop table posts;
 -- +migrate StatementEnd
