4c6612b0103731ad1f9d60748c6c95f694b013bc
Dathan Bennett <dathan@udacity.com>
2018-08-14 20:20:31 -0700

    Wire up CreateUserHandler to the API

--- handler/users.go
+++ handler/users.go
@@ -3,8 +3,30 @@ package handler
 import (
 	"net/http"
 	"github.com/udacity/go-errors"
+	"encoding/json"
+	"io/ioutil"
+	"github.com/udacity/migration-demo/db/dal"
 )
 
+type CreateUserRequest struct {
+	Id int `json:"id"`
+	DisplayName string `json:"display_name"`
+}
+
 func RegisterUserEndpoint(request *http.Request, vars map[string]string) ([]byte, int, error) {
+	var reqObject CreateUserRequest
+
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		return nil, 0, errors.WithRootCause(errors.HTTPError, err)
+	}
+	err = json.Unmarshal(body, &reqObject)
+	if err != nil {
+		return nil, 0, errors.WithRootCause(errors.HTTPBadRequestError, err)
+	}
+
+	// TODO: insert into DB
+	dal.NewUsersDAL()
+
 	return nil, 0, errors.NotImplementedError
 }
